8ff86c97baefe0585bece09503ecbc236d3e1e31
kamilpociot <kamil.pociot@me.com>
2018-09-13 18:37:32 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import './App.css';
+
+import Axios from 'axios';
+import logo from './logo.svg';
+
+class App extends React.Component<any, any> {
+  public state = {
+    response: ''
+  };
+
+  public componentDidMount() {
+    this.getString();
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          {this.state.response}
+        </p>
+      </div>
+    );
+  }
+
+  private getString() {
+    Axios
+      .get(
+        "https://accounting-kpociot.herokuapp.com/hello",
+        { headers: { 'crossOrigin': true }}
+      )
+      .then((response) => {
+        this.setState({ response: response.data });
+      })
+  }
+}
+
+export default App;
